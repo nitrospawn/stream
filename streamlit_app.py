216b21bd7ae0895852ef507c035fbd28964eb042
Ibe_chukwudi <nitrospawn@gmail.com>
2023-08-18 22:06:03 +0100

    Update streamlit_app.py
    get crpto price

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import requests
+
+def get_crypto_price(crypto):
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={crypto}&vs_currencies=USD"
+    response = requests.get(url)
+    data = response.json()
+    price = data[crypto]['usd']
+    return price
 
